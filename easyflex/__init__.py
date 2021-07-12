bc689936483f33a3dab5b82c67d1a3f7b49cc418
Melvin Folkers <melvin@yellowstacks.nl>
2021-07-12 11:10:52 +0200

    no audit on imported classes

--- easyflex/__init__.py
+++ easyflex/__init__.py
@@ -1,8 +1,1 @@
-import logging
-from easyflex.easyflex import Easyflex
-
-logging.basicConfig(
-    format="%(asctime)s.%(msecs)03d [%(levelname)-5s] [%(name)s] - %(message)s",
-    datefmt="%Y-%m-%d %H:%M:%S",
-    level=logging.INFO,
-)
+from easyflex.easyflex import Easyflex  # noqa
