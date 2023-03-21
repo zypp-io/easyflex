afefef79e4a7066a00e7fec9accd3dccb9b036d7
Tim van der Heijden <tim@zypp.io>
2023-03-21 17:09:35 +0100

    Disable argument for tqdm

--- easyflex/easyflex.py
+++ easyflex/easyflex.py
@@ -95,6 +95,7 @@ class Easyflex:
         velden: list = None,
         max_rows: int = 5000,
         inherit_datatypes: bool = False,
+        disable: bool = False,
     ) -> pd.DataFrame:
         """
         De query functie bevraagt de easyflex API op basis van de gekozen module.
@@ -113,6 +114,9 @@ class Easyflex:
         inherit_datatypes: bool
            default = True. geeft aan of de datatypes van de Easyflex dataservices worden aangehouden
            Als deze op False staat, worden geen datatypes omgezet (alles string).
+        disable  : bool, optional
+            Whether to disable the entire progressbar wrapper
+            [default: False].
 
         Returns
         -------
@@ -123,7 +127,7 @@ class Easyflex:
 
         data_list = []
 
-        for administratie in tqdm(self.administraties, desc=f"importing {module}"):
+        for administratie in tqdm(self.administraties, desc=f"importing {module}", disable=disable):
             df = self.request_data(module, administratie, parameters, velden, max_rows, inherit_datatypes)
             data_list.append(df)
         data = pd.concat(data_list, axis=0, sort=False, ignore_index=True)
